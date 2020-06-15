4448dcb633376a561e3c280d0dbbe1df7147f52d
= <=>
2020-06-15 09:52:25 +0900

    edit

---
+++ src/com/view/AnswerController.java
@@ -0,0 +1,126 @@
+package com.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.main.MainApp;
+import com.vo.GameUser;
+import com.vo.Room;
+import com.vo.User2;
+
+import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AnswerController implements Initializable{
+	
+	@FXML
+	private Label word_num1;
+	@FXML
+	private Label word_num2;
+	@FXML
+	private Label word_num3;
+	@FXML
+	private Label word_num4;
+	@FXML
+	private Label word_num5;
+	@FXML
+	private ProgressBar bar;
+	@FXML
+	private Button send;
+	@FXML
+	private Canvas canvas;
+	@FXML
+	private TableView<GameUser> playerlist;
+	@FXML
+	private TableColumn<GameUser, String> player;
+	@FXML
+	private TextField inputchat;
+	@FXML
+	private TextArea chat;
+	@FXML
+	private Label user;
+	@FXML
+	private Button match;
+	@FXML
+	private TextField inputanswer;
+	
+	@FXML
+	private Button out;
+	
+	private String userturn = "jihye";
+	
+	private MainApp mainApp;
+	
+	private String allText = "";
+	
+	private Room room;
+	
+	private User2 user2;
+	
+	public void Turn() {
+		
+		user.setText(userturn);
+		
+	}
+	public void clickSend() {
+		chat.appendText(inputchat.getText() + "\n");
+		inputchat.setText("");
+	}
+	public void clickanswer() {
+		chat.appendText(inputchat.getText() + "\n");
+		inputanswer.setText("");
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		player.setCellValueFactory(cellData -> cellData.getValue().getPlayer());
+		
+		word_num2.setText("?");
+		word_num3.setText("?");
+		word_num4.setText("?");
+		
+		Turn();
+	
+		Task<Boolean> task = new Task<Boolean>() {
+			@Override
+			protected Boolean call() throws Exception {
+				boolean flag = false;
+				for (int i = 0; i<=50; i++) {
+					updateProgress(i, 50);
+					Thread.sleep(130);
+					flag = true;
+				}
+				return flag;
+			}
+		};
+		bar.progressProperty().bind(task.progressProperty());
+		Thread thread = new Thread(task);
+		thread.start();
+	}
+
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+
+		playerlist.setItems(mainApp.getGameUsers());
+	
+
+		
+	}
+	
+	@FXML
+	private void exitApp() {
+		System.exit(0);
+	}
+}
+
+
---
+++ src/com/view/RoomListController.java
@@ -0,0 +1,118 @@
+//package com.view;
+//
+//import com.main.MainApp;
+//import com.vo.Room;
+//import com.vo.User2;
+//
+//import javafx.event.EventHandler;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TableColumn;
+//import javafx.scene.control.TableView;
+//import javafx.scene.input.MouseEvent;
+//import javafx.stage.Stage;
+//
+//public class RoomListController {
+//	@FXML
+//	private TableView<Room> roomList;
+//	@FXML
+//	private TableColumn<Room, Integer> roomNo;
+//	@FXML
+//	private TableColumn<Room, String> roomName;
+//	@FXML
+//	private TableColumn<Room, String> userCnt;
+//	@FXML
+//	private TableColumn<Room, String> enterButton;
+//	@FXML
+//	private Label noticeLabel;
+//	@FXML
+//	private TableView<User2> userList;
+//	@FXML
+//	private TableColumn<User2, String> userName;
+//	@FXML
+//	private Button galleryButton;
+//	@FXML
+//	private Button createRoomButton;
+//	@FXML
+//	private Button randomMatchingButton;
+//	
+//	private MainApp mainApp;
+//	 
+//	private Stage roomListStage;
+//	
+//	private User2 user;
+//	
+//	public RoomListController() {
+//		super();
+//	}
+//	
+//	private Room room = new Room(1, "제목01");
+//	
+//	
+//	
+//	@FXML
+//	private void initialize() {
+//		roomNo.setCellValueFactory(cellData -> cellData.getValue().getNo().asObject());
+//		roomName.setCellValueFactory(cellData -> cellData.getValue().getName());
+//		userCnt.setCellValueFactory(cellData -> cellData.getValue().getUserCnt());
+//		enterButton.setCellValueFactory(cellData -> cellData.getValue().getEnterButton());
+//		noticeLabel.setText("빠른 실행을 위해서는 랜덤매칭을 눌러주세요.");
+//		userName.setCellValueFactory(cellData -> cellData.getValue().getNickname());
+//		
+//		roomList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+//		     @Override
+//		     public void handle(MouseEvent event) {
+//		          if(event.getClickCount()>1) {
+//		                 mainApp.initWaitingRoom(user, room);
+//		          }
+//		     }
+//		});
+//	}
+//	
+//	public void setMainApp(MainApp mainApp) {
+//		this.mainApp = mainApp;
+//		
+//		roomList.setItems(mainApp.getRoomData());
+//		userList.setItems(mainApp.getUserListOfRoomList());
+//
+//		userName.setText("접속자 (" + userList.getItems().size() + "명)");
+//	}
+//	
+//	public Stage getRoomListStage() {
+//		return roomListStage;
+//	}
+//
+//	public void setRoomListStage(Stage roomListStage) {
+//		this.roomListStage = roomListStage;
+//	}
+//
+//	public User2 getUser() {
+//		return user;
+//	}
+//
+//	public void setUser(User2 user) {
+//		this.user = user;
+//	}
+//
+//	@FXML
+//	private void goToGallery() {
+//		
+//	}
+//	@FXML
+//	private void createRoom() {
+//		mainApp.initSetting(user);
+//	}
+//	@FXML
+//	private void randomMatch() {
+//		
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//}
