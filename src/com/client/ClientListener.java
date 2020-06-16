c8d92e5a8cb1f224ce6a44433710a3bddf9cf57f
kim-dabin <pippisgoing@gmail.com>
2020-06-16 12:21:47 +0900

    dabin

--- src/com/client/ClientListener.java
+++ src/com/client/ClientListener.java
@@ -69,7 +69,7 @@ public class ClientListener implements Runnable {
 			clientIP = socket.getLocalAddress().toString();
 			System.out.println("ip를 가져옵니다" + clientIP);
 			System.out.println(nickname);
-			User client = new User(clientIP, nickname, Status.CONNECTED,oos);
+			User client = new User(clientIP, nickname, Status.CONNECTED);
 			oos.writeObject(client);
 			System.out.println("is connected the server socket");
 
